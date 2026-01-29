5799a480128f5fa4384aac9368b839eeb005030a
Dư Thanh Được <duthanhduoc@gmail.com>
2026-01-29 22:24:47 +0700

    Viết markup với JSX

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,11 +1,24 @@
 const HelloWorld = () => {
   return (
     <>
-      <h2>Hello, World!</h2>
-      <h2>Hello, World!</h2>
+      <div>
+        <h1>Hedy Lamarr's Todos</h1>
+        <img
+          src='https://i.imgur.com/yXOvdOSs.jpg'
+          alt='Hedy Lamarr'
+          className='photo'
+        />
+        <hr />
+        <ul>
+          <li>Invent new traffic lights</li>
+          <li>Rehearse a movie scene</li>
+          <li>Improve the spectrum technology</li>
+        </ul>
+      </div>
     </>
   )
 }
+
 export default HelloWorld
 
 export function HelloKitty() {
