a53d52179e81f9dc9e1de7ed3e2281a257f480c9
Dư Thanh Được <duthanhduoc@gmail.com>
2026-03-09 23:19:15 +0700

    Keep component pure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import { Fragment } from 'react/jsx-runtime'
-import './App.css'
 
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Subrahmanyan Chandrasekhar: astrophysicist',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist',
-]
-
-function App() {
-  return (
-    <ul>
-      {people.map((person, index) => (
-        <Fragment key={index}>
-          <div>Số thứ tự: {index + 1}</div>
-          <li>{person}</li>
-        </Fragment>
-      ))}
-      {/* <li>Creola Katherine Johnson: mathematician</li>
-      <li>Mario José Molina-Pasquel Henríquez: chemist</li>
-      <li>Mohammad Abdus Salam: physicist</li>
-      <li>Percy Lavon Julian: chemist</li>
-      <li>Subrahmanyan Chandrasekhar: astrophysicist</li> */}
-    </ul>
-  )
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.',
+  ],
 }
 
-export default App
+export default function Poem() {
+  return (
+    <article>
+      {poem.lines.map((line, index) => (
+        <Fragment key={index}>
+          {index > 0 && <hr />}
+          <p>{line}</p>
+        </Fragment>
+      ))}
+    </article>
+  )
+}
