86c3e02ec137895fbd88e14bfbff0629c81c2a0d
Dư Thanh Được <duthanhduoc@gmail.com>
2026-05-04 23:32:12 +0700

    Công dụng của strictmode và cleanup function

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import { useEffect, useState } from 'react'
 
+const Child = () => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('Log mỗi 1 giây')
+    }, 1000)
+
+    // Cleanup function: Dùng khi lắng nghe sự kiện, hoặc khi sử dụng setInterval, setTimeout,... để tránh bị rò rỉ bộ nhớ
+    // Nó sẽ chạy khi mà component Child bị unmount (bị tắt)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+  return <div>Child</div>
+}
+
 export default function App() {
   const [count, setCount] = useState(0)
   const [show, setShow] = useState(true)
 
   // useEffect(() => {
-  //   // Effect sẽ chạy 1 lần duy nhất sau khi component được render lần đầu tiên
-  //   // Đa số 90% trường hợp sẽ dùng để fetch API
-  //   console.log('Chỉ log 1 lần duy nhất thôi')
+  //   console.log('Log 1 lần duy nhất khi component được render lần đầu tiên')
   // }, [])
 
-  // useEffect(() => {
-  //   // Effect sẽ chạy mỗi khi component được render lại
-  //   // Trường hợp này hiếm khi được sử dụng
-  //   console.log('Log mỗi khi component được render lại, kể cả lần đầu tiên')
-  // })
-
-  useEffect(() => {
-    // Chạy sau khi App Component render lần đầu tiên
-    // Và chạy sau khi mỗi lần show thay đổi
-    console.log('Hehe')
-  }, [show, count])
-
-  console.log('Render')
+  // console.log('Render')
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Count: {count}</button>
       <button onClick={() => setShow(!show)}>Show: {show.toString()}</button>
+      {show && <Child />}
     </div>
   )
 }
